6d5a4fb4f7efa808e25eb999ec835a548f031e64
93jm <65124480+93jm@users.noreply.github.com>
2021-03-02 20:27:15 +0900

    MODIFY : 초기세팅 재 수정

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return <div>발입니다</div>;
+  }
+}
+
+export default Footer;
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return <div>네비바입니다</div>;
+  }
+}
+
+export default Nav;
