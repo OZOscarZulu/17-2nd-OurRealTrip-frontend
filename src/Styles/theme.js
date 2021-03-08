021430905605803a9ed9e56723933e5d17c1e80e
93jm <65124480+93jm@users.noreply.github.com>
2021-03-08 10:04:48 +0900

    MODIFY : theme 파일 작성

--- src/Styles/theme.js
+++ src/Styles/theme.js
@@ -1,11 +1,25 @@
 import { css } from 'styled-components';
 
 const Color = {
-  mainColor: '#51ABF3',
+  blue: {
+    light: '#51ABF3',
+    dark: '#0095F6',
+  },
+  gray: {
+    light: '#E9E9E9',
+    medium: '#9B9898',
+    dark: '#343A40',
+  },
 };
+
+const Flexcenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const theme = {
   Color,
+  Flexcenter,
 };
-
 export default theme;
